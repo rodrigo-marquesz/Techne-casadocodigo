9e5bfe2b0a8c7bc5157359fb58b7e5f56ec00869
Rodrigo Zamora <rodrigojr907@gmail.com>
2022-12-11 18:11:29 -0300

    Delete target directory

--- target/casadocodigo/resources/js/analytics-tags.js
+++
@@ -1,8 +0,0 @@
-_urq.push(['_resourcesLoaded', 'TrackingPixels', (function() {
-	var urls = [];
-
-	urls.push('https://cdw-dcl.userreport.com/gs/init/pixel.gif');
-	urls.push(location.protocol + '//server.adformdsp.net/serving/cookie/match/?party=1001&cid={RespondentId}&Today={Date}');
-
-	return urls;
-})()]);
